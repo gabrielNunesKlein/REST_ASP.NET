60a6a74217adc8a3b5ac0f6a18775f032b79c13b
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-11-29 21:39:59 -0300

    Calculadora REST

---
+++ 002_REST_API.NET_Calculadora_REST/001_REST_API.NET_Criando_Projeto/Controllers/CalculatorController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _001_REST_API.NET_Criando_Projeto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult sum(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = convertToDecimal(firstNumber) + convertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult sub(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sub = convertToDecimal(firstNumber) - convertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult mult(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var mult = convertToDecimal(firstNumber) * convertToDecimal(secondNumber);
+                return Ok(mult.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult media(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var media = convertToDecimal(firstNumber) + convertToDecimal(secondNumber) / 2;
+                return Ok(media.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("raiz2/{firstNumber}")]
+        public IActionResult raiz2(string firstNumber)
+        {
+            if (isNumeric(firstNumber))
+            {
+                var num = convertToDecimal(firstNumber);
+                var raiz = Math.Sqrt((double)num);
+                return Ok(raiz.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal convertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number
+                );
+            return isNumber;
+        }
+    }
+}
