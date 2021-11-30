93ba96823816757d9b0cb4600d9da201d52b83d5
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-11-29 23:03:29 -0300

    4 verbos principais

---
+++ 003_REST_API.NET_4_Verbos_Principais/001_REST_API.NET_Criando_Projeto/Services/IPersonService.cs
@@ -0,0 +1,21 @@
+﻿using _001_REST_API.NET_Criando_Projeto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _001_REST_API.NET_Criando_Projeto.Services.Implementation
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+    }
+}
---
+++ 003_REST_API.NET_4_Verbos_Principais/001_REST_API.NET_Criando_Projeto/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,70 @@
+﻿using _001_REST_API.NET_Criando_Projeto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _001_REST_API.NET_Criando_Projeto.Services.Implementation
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MokingPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Gabriel",
+                LastName = "Klein",
+                Address = "Rua A",
+                Gender = "Mile"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MokingPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "firstName" + i,
+                LastName = "lastName" + i,
+                Address = "Rua" + i,
+                Gender = "Mile"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
